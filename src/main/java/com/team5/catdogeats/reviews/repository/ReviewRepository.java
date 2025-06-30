73db1aa2f695b294bfd99e9f841b5237cf36b8cb
KimS1Yong <sdragon0928@naver.com>
2025-06-30 13:01:24 +0900

    refactor: 불필요한 import문 제거

--- src/main/java/com/team5/catdogeats/reviews/repository/ReviewRepository.java
+++ src/main/java/com/team5/catdogeats/reviews/repository/ReviewRepository.java
@@ -1,9 +1,6 @@
 package com.team5.catdogeats.reviews.repository;
 
 import com.team5.catdogeats.reviews.domain.Reviews;
-import com.team5.catdogeats.users.domain.mapping.Buyers;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
