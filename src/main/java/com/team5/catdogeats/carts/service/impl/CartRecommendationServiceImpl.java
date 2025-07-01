2bf8ce50869f08c4c7ef269cdb897c76f212eae8
AndBlack99 <jwoo1687@icloud.com>
2025-07-01 17:11:04 +0900

    fix: import 중복 제거

--- src/main/java/com/team5/catdogeats/carts/service/impl/CartRecommendationServiceImpl.java
+++ src/main/java/com/team5/catdogeats/carts/service/impl/CartRecommendationServiceImpl.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
