8e2620f58705804bd57a0967c436419b1e83fcb7
AndBlack99 <jwoo1687@icloud.com>
2025-07-02 15:24:46 +0900

    fix : Repository 어노테이션 삭제

--- src/main/java/com/team5/catdogeats/orders/repository/OrderItemRepository.java
+++ src/main/java/com/team5/catdogeats/orders/repository/OrderItemRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 // 주문 상품 Repository
 // 결제 완료 시 구매한 상품 정보 조회를 위해 사용
-@Repository
 public interface OrderItemRepository extends JpaRepository<OrderItems, String> {
 
     // 특정 주문의 주문 상품들 조회
