80bdb3a7deb983c368ee7d04837f2d72621ecf91
YoungBeen <rhdudqls73@gmail.com>
2025-07-03 18:09:47 +0900

    "refactor : 복구"

--- src/main/java/com/team5/catdogeats/admins/repository/AdminSessionRepository.java
+++ src/main/java/com/team5/catdogeats/admins/repository/AdminSessionRepository.java
@@ -1,13 +1,13 @@
 package com.team5.catdogeats.admins.repository;
 
 import com.team5.catdogeats.admins.domain.dto.AdminSession;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AdminSessionRepository extends JpaRepository<AdminSession, String> {
+public interface AdminSessionRepository extends CrudRepository<AdminSession, String> {
 
     Optional<AdminSession> findByAdminId(String adminId);
 
