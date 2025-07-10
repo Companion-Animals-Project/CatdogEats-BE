e1915b1158c9dbf8f0bdb678b8e1406fc3b28f5c
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-07-10 15:48:56 +0900

    fix: import 문 추가

--- src/main/java/com/team5/catdogeats/orders/client/LogisticsClient.java
+++ src/main/java/com/team5/catdogeats/orders/client/LogisticsClient.java
@@ -1,5 +1,6 @@
 package com.team5.catdogeats.orders.client;
 
+import com.team5.catdogeats.orders.dto.response.TrackingResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
