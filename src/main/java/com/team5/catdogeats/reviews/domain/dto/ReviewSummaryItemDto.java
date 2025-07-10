2b0e027c9bc64fb93c38ff1acc81ec2e73ecc510
KimS1Yong <sdragon0928@naver.com>
2025-07-10 10:13:50 +0900

    feat: batch기능 위한 dto 추가

--- src/main/java/com/team5/catdogeats/reviews/domain/dto/ReviewSummaryItemDto.java
+++ src/main/java/com/team5/catdogeats/reviews/domain/dto/ReviewSummaryItemDto.java
@@ -1,6 +1,10 @@
 package com.team5.catdogeats.reviews.domain.dto;
 
+import java.util.List;
+
 public record ReviewSummaryItemDto(
-        String summary
+        String summary,
+        List<String> mainPoints,
+        List<String> keywords
 ) {
 }
