01012a0b38095add4ea84421eeaca369c53d013d
KimS1Yong <sdragon0928@naver.com>
2025-07-10 12:01:53 +0900

    refactor: 오타 수정

--- src/main/java/com/team5/catdogeats/reviews/repository/ReviewClassificationLLMDogFinishedRepository.java
+++ src/main/java/com/team5/catdogeats/reviews/repository/ReviewClassificationLLMDogFinishedRepository.java
@@ -2,7 +2,6 @@ package com.team5.catdogeats.reviews.repository;
 
 import com.team5.catdogeats.products.domain.Products;
 import com.team5.catdogeats.reviews.domain.Reviews;
-import com.team5.catdogeats.reviews.domain.mapping.ReviewClassificationLLMCatFinished;
 import com.team5.catdogeats.reviews.domain.mapping.ReviewClassificationLLMDogFinished;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
