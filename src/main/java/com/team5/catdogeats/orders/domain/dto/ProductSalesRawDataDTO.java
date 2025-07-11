8b4d1a677e1c3207c58a49d96c00e2a9c4ca76e4
SoowanPark <67948990+cake0420@users.noreply.github.com>
2025-07-12 05:05:19 +0900

    Revert "[feat] 정산 관련 기능 "

--- src/main/java/com/team5/catdogeats/orders/domain/dto/ProductSalesRawDataDTO.java
+++
@@ -1,8 +0,0 @@
-package com.team5.catdogeats.orders.domain.dto;
-
-public record ProductSalesRawDataDTO(
-        String productId,
-        String productName,
-        Long totalAmount,
-        Long quantity
-) {}
