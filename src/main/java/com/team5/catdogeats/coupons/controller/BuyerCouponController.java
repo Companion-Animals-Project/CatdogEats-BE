221b307564207d8a544f2c9d25d25ac22ee467dc
soowan <nick6767676767@gmail.com>
2025-07-12 16:19:46 +0900

    fix: dto 이름 수정

--- src/main/java/com/team5/catdogeats/coupons/controller/BuyerCouponController.java
+++ src/main/java/com/team5/catdogeats/coupons/controller/BuyerCouponController.java
@@ -6,7 +6,7 @@ import com.team5.catdogeats.coupons.domain.dto.BuyerCreateCouponRequestDTO;
 import com.team5.catdogeats.coupons.domain.enums.CouponFilterType;
 import com.team5.catdogeats.coupons.exception.DuplicateCouponException;
 import com.team5.catdogeats.coupons.service.BuyerCouponService;
-import com.team5.catdogeats.global.dto.ApiResponse;
+import com.team5.catdogeats.global.dto.APIResponse;
 import com.team5.catdogeats.global.enums.ResponseCode;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -28,33 +28,33 @@ public class BuyerCouponController {
     private final BuyerCouponService buyerCouponService;
 
     @GetMapping
-    public ResponseEntity<ApiResponse<BuyerCouponListResponseDTO>> getCoupons(@AuthenticationPrincipal UserPrincipal userPrincipal,
-                                                                                   @RequestParam CouponFilterType filter,
-                                                                                   @RequestParam(defaultValue = "0") int page) {
+    public ResponseEntity<APIResponse<BuyerCouponListResponseDTO>> getCoupons(@AuthenticationPrincipal UserPrincipal userPrincipal,
+                                                                              @RequestParam CouponFilterType filter,
+                                                                              @RequestParam(defaultValue = "0") int page) {
         try {
-            return ResponseEntity.ok(ApiResponse.success(ResponseCode.SUCCESS, buyerCouponService.getBuyerCoupons(userPrincipal,filter,page,10)));
+            return ResponseEntity.ok(APIResponse.success(ResponseCode.SUCCESS, buyerCouponService.getBuyerCoupons(userPrincipal,filter,page,10)));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error(ResponseCode.INTERNAL_SERVER_ERROR));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(APIResponse.error(ResponseCode.INTERNAL_SERVER_ERROR));
         }
 
     }
 
 
     @PostMapping
-    public ResponseEntity<ApiResponse<Void>> createCoupon(@AuthenticationPrincipal UserPrincipal userPrincipal,
-                                                    @Valid @RequestBody BuyerCreateCouponRequestDTO dto) {
+    public ResponseEntity<APIResponse<Void>> createCoupon(@AuthenticationPrincipal UserPrincipal userPrincipal,
+                                                          @Valid @RequestBody BuyerCreateCouponRequestDTO dto) {
         if (userPrincipal == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error(ResponseCode.UNAUTHORIZED));
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(APIResponse.error(ResponseCode.UNAUTHORIZED));
         }
         try {
             buyerCouponService.createCoupon(userPrincipal, dto);
-            return ResponseEntity.ok(ApiResponse.success(ResponseCode.SUCCESS));
+            return ResponseEntity.ok(APIResponse.success(ResponseCode.SUCCESS));
         } catch (NoSuchElementException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.error(ResponseCode.INVALID_INPUT_VALUE));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(APIResponse.error(ResponseCode.INVALID_INPUT_VALUE));
         } catch (DuplicateCouponException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(ApiResponse.error(ResponseCode.DUPLICATE_COUPON_CODE));
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(APIResponse.error(ResponseCode.DUPLICATE_COUPON_CODE));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error(ResponseCode.INTERNAL_SERVER_ERROR));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(APIResponse.error(ResponseCode.INTERNAL_SERVER_ERROR));
         }
     }
 }
