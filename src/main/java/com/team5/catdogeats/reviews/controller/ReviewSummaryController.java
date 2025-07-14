17ea1dce6378226fa3dfd4a6067eea83133eb4ce
KimS1Yong <sdragon0928@naver.com>
2025-07-14 10:21:32 +0900

    refactor: 병합 중 바뀐 global부분에 맞추기

--- src/main/java/com/team5/catdogeats/reviews/controller/ReviewSummaryController.java
+++ src/main/java/com/team5/catdogeats/reviews/controller/ReviewSummaryController.java
@@ -1,7 +1,7 @@
 package com.team5.catdogeats.reviews.controller;
 
 import com.fasterxml.jackson.core.JsonParseException;
-import com.team5.catdogeats.global.dto.ApiResponse;
+import com.team5.catdogeats.global.dto.APIResponse;
 import com.team5.catdogeats.global.enums.ResponseCode;
 import com.team5.catdogeats.reviews.domain.dto.ReviewSummaryResponseDto;
 import com.team5.catdogeats.reviews.service.ReviewSummaryService;
@@ -21,23 +21,23 @@ public class ReviewSummaryController {
     private final ReviewSummaryService reviewSummaryService;
 
     @GetMapping("/{productNumber}")
-    public ResponseEntity<ApiResponse<ReviewSummaryResponseDto>> getReviewSummary(@PathVariable Long productNumber) {
+    public ResponseEntity<APIResponse<ReviewSummaryResponseDto>> getReviewSummary(@PathVariable Long productNumber) {
         try {
             ReviewSummaryResponseDto summary = reviewSummaryService.getReviewSummaryByProductNumber(productNumber);
-            return ResponseEntity.ok(ApiResponse.success(ResponseCode.SUCCESS, summary));
+            return ResponseEntity.ok(APIResponse.success(ResponseCode.SUCCESS, summary));
         } catch (JsonParseException e) {
             return ResponseEntity
                     .status(ResponseCode.JSON_PARSING_FAIL.getStatus())
-                    .body(ApiResponse.error(ResponseCode.JSON_PARSING_FAIL, e.getMessage()));
+                    .body(APIResponse.error(ResponseCode.JSON_PARSING_FAIL, e.getMessage()));
         } catch (NoSuchElementException e) {
             // 상품 없거나, 검증된 리뷰 및 리뷰 요약 없을 때
             return ResponseEntity
                     .status(ResponseCode.ENTITY_NOT_FOUND.getStatus())
-                    .body(ApiResponse.error(ResponseCode.ENTITY_NOT_FOUND, e.getMessage()));
+                    .body(APIResponse.error(ResponseCode.ENTITY_NOT_FOUND, e.getMessage()));
         } catch (Exception e) {
             return ResponseEntity
                     .status(ResponseCode.INTERNAL_SERVER_ERROR.getStatus())
-                    .body(ApiResponse.error(ResponseCode.INTERNAL_SERVER_ERROR, e.getMessage()));
+                    .body(APIResponse.error(ResponseCode.INTERNAL_SERVER_ERROR, e.getMessage()));
         }
     }
 }
