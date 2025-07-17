0cf459cf1f705da690480dd7d3c9e7147589a71c
soowan <nick6767676767@gmail.com>
2025-07-18 00:46:19 +0900

    fix: 세션 policy 수정

--- src/main/java/com/team5/catdogeats/global/config/SessionConfig.java
+++ src/main/java/com/team5/catdogeats/global/config/SessionConfig.java
@@ -1,26 +1,22 @@
 package com.team5.catdogeats.global.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.http.SessionRepositoryFilter;
 
 @Configuration
 @EnableRedisHttpSession
 public class SessionConfig {
-    @Bean
-    public FilterRegistrationBean<SessionRepositoryFilter<?>> springSessionFilter(
-            SessionRepositoryFilter<?> springSessionRepositoryFilter) {
-
-        FilterRegistrationBean<SessionRepositoryFilter<?>> registration =
-                new FilterRegistrationBean<>(springSessionRepositoryFilter);
-
-        registration.addUrlPatterns("/v1/admins/**");
-
-        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
-        return registration;
-    }
+//    @Bean
+//    public FilterRegistrationBean<SessionRepositoryFilter<?>> springSessionFilter(
+//            SessionRepositoryFilter<?> springSessionRepositoryFilter) {
+//
+//        FilterRegistrationBean<SessionRepositoryFilter<?>> registration =
+//                new FilterRegistrationBean<>(springSessionRepositoryFilter);
+//
+//        registration.addUrlPatterns("/v1/admins/**");
+//
+//        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//
+//        return registration;
+//    }
 }
